814f086fd7f42ad35b86b587a2b33ea225608bc0
jiqianxiang <jiqianxiang@icubecorp.com>
2018-09-21 09:57:14 +0800

    add AddressListParser (memory and address P2P)

--- tools/caffe2fb/include/address_list_parser.h
+++ tools/caffe2fb/include/address_list_parser.h
@@ -9,20 +9,23 @@
 #define ADDRESS_LIST_PARSER_H_
 
 #include "list_entry_parser.h"
+#include "memory_list_parser.h"
 
 namespace nvdla {
 
 class AddressListParser: public ListEntryParser {
 public:
-	AddressListParser(NetParser* net);
+	AddressListParser(NetParser* net, MemoryListParser* mems);
 	virtual ~AddressListParser();
 
 	void  buildList();
-	void* getList() const;
+	const void* getList() const;
 
 
 private:
 	std::vector<ILoadable::AddressListEntry> mList;
+
+	MemoryListParser* mMemoryListParserPtr;
 };
 
 } /* namespace nvdla */
--- tools/caffe2fb/list_parser/address_list_parser.cpp
+++ tools/caffe2fb/list_parser/address_list_parser.cpp
@@ -9,8 +9,9 @@
 
 namespace nvdla {
 
-AddressListParser::AddressListParser(NetParser* net) :
-	ListEntryParser(net)
+AddressListParser::AddressListParser(NetParser* net, MemoryListParser* mems) :
+	ListEntryParser(net),
+	mMemoryListParserPtr(mems)
 {
 
 }
@@ -18,13 +19,25 @@ AddressListParser::AddressListParser(NetParser* net) :
 AddressListParser::~AddressListParser() {
 }
 
-void* AddressListParser::getList() const
+const void* AddressListParser::getList() const
 {
-
+	return (const void*)&mList;
 }
 
-void  AddressListParser::buildList()
+void AddressListParser::buildList()
 {
+	const std::vector<ILoadable::MemoryListEntry> mems = mMemoryListParserPtr->getList();
+
+	for (int i=0; i<mems.size(); i++) {
+		ILoadable::AddressListEntry addr;
+
+		addr.id = i;
+		addr.mem_id = mems[i].id;
+		addr.size = mems[i].size;
+		addr.offset = 0;
+
+		mList.push_back(addr);
+	}
 
 }
 
